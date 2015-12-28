3c5ed36f9410cf9a70025f0900e565567576766d
iluz <iluz@ciandt.com>
2015-12-28 16:11:01 -0200

    Display joke returned from Java library

---
+++ jokes/src/main/java/com/gmail/ivamsantos/JokesProvider.java
@@ -0,0 +1,32 @@
+package com.gmail.ivamsantos;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class JokesProvider {
+    private Random randomGenerator;
+
+    public JokesProvider() {
+        randomGenerator = new Random();
+    }
+
+    private static List<String> jokes = Arrays.asList(
+            "Funny joke 1",
+            "Funny joke 2",
+            "Funny joke 3",
+            "Funny joke 4",
+            "Funny joke 5",
+            "Funny joke 6",
+            "Funny joke 7",
+            "Funny joke 8",
+            "Funny joke 9",
+            "Funny joke 10"
+    );
+
+    public String getJoke() {
+        int randomIndex = randomGenerator.nextInt(jokes.size());
+
+        return jokes.get(randomIndex);
+    }
+}
